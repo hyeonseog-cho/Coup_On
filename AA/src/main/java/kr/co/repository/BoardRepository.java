11f7242911370c461e8caaaef011176757f58557
yoy2655-patch-1-1 <dbstls87@gmail.com>
2023-03-03 11:05:08 +0900

    test2223333

--- AA/src/main/java/kr/co/repository/BoardRepository.java
+++ AA/src/main/java/kr/co/repository/BoardRepository.java
@@ -6,5 +6,5 @@ import kr.co.entity.Board;
 
 // Repository -> DB연결을 담당하지만 ServiceImpl에서 Repository의 함수를 이용하기 때문에 공백란으로 남겨둠
 public interface BoardRepository extends JpaRepository<Board, String> {
-
+//테슽222222333
 }
