cbc69aa8ea83f76a144beddbea7f22c2fba6ef69
JHS0425 <mad02972@gmail.com>
2023-03-03 10:26:56 +0900

    ㅁ

--- AA/src/main/java/kr/co/repository/BoardRepository.java
+++ AA/src/main/java/kr/co/repository/BoardRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import kr.co.entity.Board;
 
+// Repository -> DB연결을 담당하지만 ServiceImpl에서 Repository의 함수를 이용하기 때문에 공백란으로 남겨둠
 public interface BoardRepository extends JpaRepository<Board, String> {
 
 }
